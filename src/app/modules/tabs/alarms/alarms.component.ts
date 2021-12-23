da53df14b36ac56eb0d6b7af9165fdd151788fa8
Amartya Aishwarya <amartya.aishwarya@accolitedigital.com>
2021-12-23 13:43:36 +0530

    alarms empty comp

--- src/app/modules/tabs/alarms/alarms.component.ts
+++ src/app/modules/tabs/alarms/alarms.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges, Input } from '@angular/core';
+import { StartUpService } from "src/app/helpers/services";
+import { ExposureService } from 'src/app/helpers/services/exposure.service';
+import { DataService } from 'src/app/helpers/services/network-graph/data.service';
 
 @Component({
   selector: 'app-alarms',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlarmsComponent implements OnInit {
 
-  constructor() { }
+  @Input() selectedNodeInventoryDetails: any ; 
+  nodeDetailsProperties: any;
+  
+  constructor(private startUp: StartUpService,
+    private exposureService: ExposureService,
+    private dataSvc: DataService) {
+     let exposureUrl= this.startUp.getConfig('exposure');
+     let Url = new URL(exposureUrl);
+   }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+      this.nodeDetailsProperties = this.selectedNodeInventoryDetails;
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+    }
 
 }
